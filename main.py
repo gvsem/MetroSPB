675316843247bef922db6c18213d9beb1a3ebb75
Georgii Semenov <themrgeorgeous@gmail.com>
2021-07-07 19:10:54 +0300

    Added directions

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ from PIL import ImageDraw
 from transliterate import translit
 import json
 
-from metro import renderLineLogo, lineColors, renderStationTable, renderStationTableFromJson
+from metro import renderLineLogo, lineColors, renderStationTable, renderStationTableFromJson, renderDirectionTable, \
+    renderDirectionSplitTable, stationToEnglish, renderWayOut
+
+s = "Шушары"
+#renderDirectionTable(s, translit(s, "ru", reversed=True), lineColors[4]).show()
+s = "Спасская"
+#renderWayOut(1300, 200).show()
+#renderDirectionSplitTable(s, stationToEnglish(s), 4, False).show()
+s = "Купчино"
+#renderDirectionSplitTable(s, stationToEnglish(s), 2, False).show()
+#exit(0)
+
 
 for i in range(len(lineColors)):
     renderLineLogo(i+1).save("out/lines/line" + str(i + 1) + ".png", format='PNG')
@@ -40,6 +51,42 @@ with open("data.json", "r", encoding='utf-8') as file:
             offset += st.size[1] + margin
 
         i.save("out/views/line" + str(line_i) + ".png")
+
+
+with open("data.json", "r", encoding='utf-8') as file:
+    data = json.load(file)
+    line_i = 0
+    for line in data:
+        line_i += 1
+        margin = 0
+        offset = margin
+        n = 1300
+        m = margin + (200 + margin) * len(line)
+        i = Image.new('RGB', (n, m))
+
+        for station in line:
+            st = renderStationTableFromJson(station, line_i)
+            i.paste(st, (0, offset))
+            offset += st.size[1] + margin
+
+        i.save("out/views/line" + str(line_i) + ".png")
+
+
+
+with open("data.json", "r", encoding='utf-8') as file:
+    data = json.load(file)
+    line_i = 0
+    for line in data:
+        line_i += 1
+        s = line[0]["name"]
+        renderDirectionTable(s, stationToEnglish(s), lineColors[line_i - 1]).save("out/directions/line" + str(line_i) + "north.png")
+        renderDirectionSplitTable(s, stationToEnglish(s), line_i, True).save("out/directions/line" + str(line_i) + "northRight.png")
+        renderDirectionSplitTable(s, stationToEnglish(s), line_i, False).save("out/directions/line" + str(line_i) + "northLeft.png")
+        s = line[-1]["name"]
+        renderDirectionTable(s, stationToEnglish(s), lineColors[line_i - 1]).save("out/directions/line" + str(line_i) + "south.png")
+        renderDirectionSplitTable(s, stationToEnglish(s), line_i, True).save("out/directions/line" + str(line_i) + "southRight.png")
+        renderDirectionSplitTable(s, stationToEnglish(s), line_i, False).save("out/directions/line" + str(line_i) + "southLeft.png")
+
 
 # s = "Площадь Восстания"
 # renderStationTable([1, 3], s, translit(s, "ru", reversed=True)).save("out")
--- metro.py
+++ metro.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 from PIL import ImageFont
 from PIL import ImageDraw
 from transliterate import translit
@@ -13,7 +13,19 @@ colorInfo = (27, 22, 151, 255)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-def renderLineLogo(number):
+def stationToEnglish(t):
+    # translit(s, "ru", reversed=True)
+    symbols = (u"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ",
+               (*list(u'abvgdee'), 'zh', *list(u'zijklmnoprstuf'), 'kh', 'z', 'ch', 'sh', 'sh', '',
+                'y', '', 'e', 'yu','ya', *list(u'ABVGDEE'), 'ZH',
+                *list(u'ZIJKLMNOPRSTUF'), 'KH', 'Z', 'CH', 'SH', 'SH', *list(u'_Y_E'), 'YU', 'YA', ' '))
+
+    coding_dict = {source: dest for source, dest in zip(*symbols)}
+    translate = lambda x: ''.join([coding_dict[i] for i in x])
+    return translate(t)
+
+
+def renderLineLogo(number, withWhiteBorder=False):
     i = Image.new('RGB', (180, 120))
     i.paste(lineColors[number - 1], [0,0,i.size[0],i.size[1]])
     draw = ImageDraw.Draw(i)
@@ -21,6 +33,8 @@ def renderLineLogo(number):
     draw.text((115, 7), str(number), (255, 255, 255), font=fontLine)
     metroLogo = Image.open("logo_white.png").resize((90, 70))
     i.paste(metroLogo, (18,26), metroLogo)
+    if withWhiteBorder:
+        i = ImageOps.expand(i,border=5,fill='white')
     return i
 
 def renderStationTable(lines, title, subtitle):
@@ -75,7 +89,177 @@ def renderStationTable(lines, title, subtitle):
 
     return i
 
+
+
+def renderDirectionTable(title, subtitle, color):
+
+    n = 1300 # 1100
+    m = 200
+    zoneOffset = 35 # 10
+    metroLogoSize = (135, 135)
+    metroLogoOffsetH = int((m - metroLogoSize[1]) / 2)
+    linesLogoSize = (145, 100)
+    linesLogoOffsetH = int((m - linesLogoSize[1]) / 2)
+    backgroundColor = color
+
+    offset = -1
+    lessFont = 1.0
+    while offset < 20:
+
+        # fontH1 = ImageFont.truetype("FRS65.ttf", int(65 * lessFont))
+        # fontH2 = ImageFont.truetype("FRS65.ttf", int(60 * lessFont))
+        fontH1 = ImageFont.truetype("FreeSet Bold.ttf", int(65 * lessFont))
+        fontH2 = ImageFont.truetype("FreeSet Bold.ttf", int(60 * lessFont))
+        # fontH1 = ImageFont.truetype("BarnaulGroteskExtraBold-Reg.ttf", int(65 * lessFont))
+        # fontH2 = ImageFont.truetype("BarnaulGroteskExtraBold-Reg.ttf", int(60 * lessFont))
+        # fontH1 = ImageFont.truetype("HelveticaNeueLTW1GBdCn.otf", int(65 * lessFont))
+        # fontH2 = ImageFont.truetype("HelveticaNeueLTW1GBdCn.otf", int(60 * lessFont))
+
+        titleWidth = fontH1.getsize(title)[0]
+        subtitleWidth = fontH2.getsize(subtitle)[0]
+        textWidth = max(titleWidth, subtitleWidth)
+        textOffset = int((titleWidth - subtitleWidth) / 2)
+        width = metroLogoSize[0] + zoneOffset + textWidth
+
+        offset = int((n - width) / 2)
+        lessFont -= 0.1
+
+
+
+    i = Image.new('RGBA', (n, m))
+    i.paste(backgroundColor, [0,0,i.size[0],i.size[1]])
+    metroLogo = Image.open("arrow_white.png").resize(metroLogoSize)
+    i.paste(metroLogo, (offset, metroLogoOffsetH), metroLogo)
+    draw = ImageDraw.Draw(i)
+
+    offset += metroLogoSize[0] + zoneOffset
+
+    draw.text((offset, 27), title, white, font=fontH1)
+    draw.text((offset + textOffset, 93), subtitle, white, font=fontH2)
+
+    return i
+
 def renderStationTableFromJson(station, line_i):
     s = station["name"]
     i = [line_i] + station["interchange"]
     return renderStationTable(i, s, translit(s, "ru", reversed=True))
+
+
+def renderWayOut(n, m):
+
+    fontH1 = ImageFont.truetype("FreeSet Bold.ttf", int(65))
+    fontH2 = ImageFont.truetype("FreeSet Bold.ttf", int(60))
+
+    title = "Выход"
+    subtitle = "Way out"
+    zoneOffset = 35 # 10
+    wayOutLogoSize = (135, 150)
+    wayOutLogoOffsetH = int((m - wayOutLogoSize[1]) / 2)
+    backgroundColor = colorInfo
+
+    titleWidth = fontH1.getsize(title)[0]
+    subtitleWidth = fontH2.getsize(subtitle)[0]
+    textWidth = max(titleWidth, subtitleWidth)
+    textOffset = int((titleWidth - subtitleWidth) / 2)
+    width = wayOutLogoSize[0] + zoneOffset + textWidth
+
+    offset = int((n - width) / 2)
+
+    i = Image.new('RGBA', (n, m))
+    i.paste(backgroundColor, [0,0,i.size[0],i.size[1]])
+    draw = ImageDraw.Draw(i)
+
+    draw.text((offset, 27), title, white, font=fontH1)
+    draw.text((offset + textOffset, 93), subtitle, white, font=fontH2)
+    offset += textWidth + zoneOffset
+
+    wayOutLogo = Image.open("wayout.png").resize(wayOutLogoSize)
+    i.paste(wayOutLogo, (offset, wayOutLogoOffsetH), wayOutLogo)
+    offset += wayOutLogoSize[0] + zoneOffset
+
+    return i
+
+
+def renderDirectionSplitTable(title, subtitle, line, outIsRight=True):
+
+    n = 1600 # 1100
+    m = 200
+    zoneOffset = 35 # 10
+    metroLogoSize = (135, 135)
+    metroLogoOffsetH = int((m - metroLogoSize[1]) / 2)
+    linesLogoSize = (165, 120)
+    linesLogoOffsetH = int((m - linesLogoSize[1]) / 2)
+    backgroundColor = lineColors[line - 1]
+
+    offset = -1
+    lessFont = 1.0
+    while offset < 20:
+
+        # fontH1 = ImageFont.truetype("FRS65.ttf", int(65 * lessFont))
+        # fontH2 = ImageFont.truetype("FRS65.ttf", int(60 * lessFont))
+        fontH1 = ImageFont.truetype("FreeSet Bold.ttf", int(65 * lessFont))
+        fontH2 = ImageFont.truetype("FreeSet Bold.ttf", int(60 * lessFont))
+        # fontH1 = ImageFont.truetype("BarnaulGroteskExtraBold-Reg.ttf", int(65 * lessFont))
+        # fontH2 = ImageFont.truetype("BarnaulGroteskExtraBold-Reg.ttf", int(60 * lessFont))
+        # fontH1 = ImageFont.truetype("HelveticaNeueLTW1GBdCn.otf", int(65 * lessFont))
+        # fontH2 = ImageFont.truetype("HelveticaNeueLTW1GBdCn.otf", int(60 * lessFont))
+
+        titleWidth = fontH1.getsize(title)[0]
+        subtitleWidth = fontH2.getsize(subtitle)[0]
+        textWidth = max(titleWidth, subtitleWidth)
+        textOffset = int((titleWidth - subtitleWidth) / 2)
+        width = metroLogoSize[1] + zoneOffset + textWidth + linesLogoSize[0] + zoneOffset
+
+        offset = int((n - width) / 2)
+        lessFont -= 0.1
+
+    # no center align needed here
+    offset = 30
+
+    i = Image.new('RGBA', (n, m))
+    i.paste(backgroundColor, [0,0,i.size[0],i.size[1]])
+    draw = ImageDraw.Draw(i)
+
+    wayN = max(600, int(0.35 * n))
+    wayOut = renderWayOut(wayN, m)
+
+    if outIsRight:
+
+        metroLogo = Image.open("arrow_white.png").resize(metroLogoSize).rotate(90)
+        i.paste(metroLogo, (offset, metroLogoOffsetH), metroLogo)
+
+        offset += metroLogoSize[0] + zoneOffset
+
+        lineLogo = renderLineLogo(line, True).resize(linesLogoSize)
+        i.paste(lineLogo, (offset, linesLogoOffsetH))
+        offset += linesLogoSize[0] + zoneOffset
+
+        draw.text((offset, 27), title, white, font=fontH1)
+        draw.text((offset, 93), subtitle, white, font=fontH2)
+
+        i.paste(wayOut, (n - wayN, 0), wayOut)
+
+        draw.line([(n - wayN, 0), (n - wayN, m)], fill="white", width=10)
+
+    else:
+
+        i.paste(wayOut, (0, 0), wayOut)
+        draw.line([(wayN, 0), (wayN, m)], fill="white", width=10)
+
+        offset = n - offset - metroLogoSize[0]
+
+        metroLogo = Image.open("arrow_white.png").resize(metroLogoSize).rotate(270)
+        i.paste(metroLogo, (offset, metroLogoOffsetH), metroLogo)
+        draw = ImageDraw.Draw(i)
+
+        offset -= zoneOffset + linesLogoSize[0]
+        lineLogo = renderLineLogo(line, True).resize(linesLogoSize)
+        i.paste(lineLogo, (offset, linesLogoOffsetH))
+
+        offset -= zoneOffset + textWidth
+
+        draw.text((offset, 27), title, white, font=fontH1)
+        draw.text((offset + textOffset, 93), subtitle, white, font=fontH2)
+
+
+    return i
